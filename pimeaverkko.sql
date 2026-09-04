cbab6d3113ca16d40179c64e29021e6b9173cc38
Kaapo Kivijarvi <kaapo.kivijarvi@samiedu365.fi>
2026-09-04 12:55:09 +0300

    Päivitetty sivustoa

--- pimeaverkko.sql
+++ pimeaverkko.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Isäntä: 127.0.0.1:3306
--- Luontiaika: 04.09.2026 klo 06:56
+-- Luontiaika: 04.09.2026 klo 09:54
 -- Palvelimen versio: 8.4.7
 -- PHP-versio 8.3.28
 
@@ -35,18 +35,16 @@ CREATE TABLE IF NOT EXISTS `groups` (
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_User_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Vedos taulusta `groups`
 --
 
 INSERT INTO `groups` (`id`, `user_id`, `name`, `description`) VALUES
-(1, 1, 'Pelit2', 'peleistä liittyviä keskusteluja'),
-(2, 1, 'Autot', 'Autoista liittyviä keskusteluja'),
-(9, 1, 'Koulu', 'kouluun liittyvää keskustelua'),
-(10, 1, '\'', '\''),
-(11, 1, 'auto', 'fgdfghs');
+(14, 7, 'Pelit', 'Peleihin liittyvää keskustelua'),
+(16, 7, 'Autot', 'Autoista liittyvää keskustelua'),
+(17, 8, 'Koirat', 'koiria');
 
 -- --------------------------------------------------------
 
@@ -65,20 +63,17 @@ CREATE TABLE IF NOT EXISTS `posts` (
   PRIMARY KEY (`id`),
   KEY `POST_FK_user_id` (`user_id`),
   KEY `POST_FK_group_id` (`group_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Vedos taulusta `posts`
 --
 
 INSERT INTO `posts` (`id`, `group_id`, `user_id`, `author`, `content`, `created_at`) VALUES
-(7, 1, 1, 'Jarkko', 'Onko fortnite maailamn suosituin peli, minun lempparini se ainakin on', '2026-08-21 09:32:59'),
-(9, 2, 1, 'Jarkko', 'lempi autoni on Toyota Yaris 1.0 litraisella koneella', '2026-08-21 09:34:24'),
-(10, 2, 1, 'Jarkko', 'mOPO ON PAREMPI KUIN AUTO', '2026-08-21 09:38:53'),
-(11, 9, 1, 'Jarkko', 'MINÄ VIHAAN KOULUA TODELLA PALJON', '2026-08-21 09:39:37'),
-(12, 10, 1, '¨ÅP08967¨\'Ä\'\'¨\'', '\'¨\'0875Q£@€$€$', '2026-08-21 09:40:03'),
-(15, 1, 1, 'Jarkko H', 'moi', '2026-08-25 09:08:43'),
-(17, 1, 6, 'testi', 'testi', '2026-09-04 06:55:30');
+(20, 14, 7, 'Kaapo', 'Fortnite on huono peli', '2026-09-04 07:34:16'),
+(21, 16, 7, 'Kaapo', 'Jarkon lempiauto on Toyota Yaris 1.0', '2026-09-04 07:35:57'),
+(22, 16, 7, 'Kaapo', 'Toyota corolla 1.4', '2026-09-04 07:36:59'),
+(23, 14, 8, 'Jarkko2', 'jarkko2', '2026-09-04 09:46:25');
 
 -- --------------------------------------------------------
 
@@ -94,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password_hash` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Vedos taulusta `user`
@@ -105,7 +100,9 @@ INSERT INTO `user` (`id`, `name`, `email`, `password_hash`) VALUES
 (2, 'Admin2', 'admin2@gmail.com', '$2y$10$BKdrN5G2M5EYrxYHPDa/muJzNDDnoI0JpkoTH9INn5435PK.P6RiW'),
 (3, 'admin', 'admin3@gmail.com', '$2y$10$UsdzOGFJzGZCgX5jpwA.aePpzgLmUGq7en0bSI/MAgz51L1H5H10y'),
 (5, 'Jarkko', 'jarkko@gmail.com', '$2y$10$YTr9IDRhnTdLYW.oYY2zZ.X9TUDMdDCQfsprJWuWCp.oYopJ11XZS'),
-(6, 'testi2', 'testi@gmail.com', '$2y$10$oPiXrOOCmEiRiHMZo0iZDuh2rWQPXmajxNFdKsPLwBcFPVL8jKgZ6');
+(6, 'testi', 'testi@gmail.com', '$2y$10$oPiXrOOCmEiRiHMZo0iZDuh2rWQPXmajxNFdKsPLwBcFPVL8jKgZ6'),
+(7, 'Kaapo', 'kaapo@gmail.com', '$2y$10$MhLzDhYQESsnyL7.xp0.Cuq3u0sVDPZbIUODlx/M/kHh3L7TajcN2'),
+(8, 'Jarkko2', 'jarkko2@gmail.com', '$2y$10$3ofJXotjTHxgysph..gj6uOobiqgLtMX.GGoBL9qZQsimd.1Cb.l6');
 
 --
 -- Rajoitteet vedostauluille
